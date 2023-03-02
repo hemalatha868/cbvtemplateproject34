8efe7866444c14184d1612c75cc2a103da87501d
hemalatha868 <ahemalatha868@gmail.com>
2023-03-02 13:33:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class CbvContext(TemplateView):
+    template_name='CbvContext.html'
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['name']='Honey'
+        context['age']=21
+        return context
+
+
+
